27c0dd1efcd3f827145976e5f5c1b8e6c094ac5c
jarethussamy <42392057+jarethussamy@users.noreply.github.com>
2025-06-23 11:53:29 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UberApp from './components/UberApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UberApp />
+    </div>
+  );
+}
+
+export default App;
